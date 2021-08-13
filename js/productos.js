a667a7bb31745b9b7f6c72c062a32d55040defdf
clarigullon <clariigullon@gmail.com>
2021-08-13 19:41:39 -0300

    avance carrito

--- js/productos.js
+++ js/productos.js
@@ -1,271 +1,270 @@
 class Producto {
-  constructor(nombre, seccion, precio, id, img) {
+  constructor(nombre, seccion, id, precio, img) {
     this.nombre = nombre;
     this.seccion = seccion;
     this.precio = precio;
     this.id = id;
     this.img = img;
-    this.stock = "En stock";
   }
 }
 const superdesayuno = new Producto(
   "Super Desayuno",
   "regalos",
+  Math.random().toString(36).substr(2, 9),
   1800,
-  1,
   "imagenesventa/desayunog-min.jpg"
 );
 const desayuno = new Producto(
   "Desayuno",
   "regalos",
+  Math.random().toString(36).substr(2, 9),
   1500,
-  2,
   "imagenesventa/desayunochic-min.jpg"
 );
 const bandejitagrande = new Producto(
   "Bandejita de tortitas x16",
   "regalos",
+  Math.random().toString(36).substr(2, 9),
   900,
-  3,
   "imagenesventa/bandejitagrande-min.jpg"
 );
 const bandejitachica = new Producto(
   "Bandejita de mini tortitas x8",
   "regalos",
+  Math.random().toString(36).substr(2, 9),
   700,
-  4,
   "imagenesventa/bandejitachica-min.jpg"
 );
 const bomba = new Producto(
   "Torta Bomba",
   "postres",
+  Math.random().toString(36).substr(2, 9),
   1300,
-  5,
   "imagenesventa/bomba-min.JPG"
 );
 const cabsha = new Producto(
   "Torta Cabsha",
   "postres",
+  Math.random().toString(36).substr(2, 9),
   1200,
-  6,
   "imagenesventa/cabsha-min.jpeg"
 );
 const rogel = new Producto(
   "Rogel",
   "postres",
+  Math.random().toString(36).substr(2, 9),
   1300,
-  7,
   "imagenesventa/rogel2.JPG"
 );
 const helada = new Producto(
   "Torta Helada",
   "postres",
+  Math.random().toString(36).substr(2, 9),
   1000,
-  8,
   "imagenesventa/helad-min.JPG"
 );
 const brownie = new Producto(
   "Brownie",
   "postres",
+  Math.random().toString(36).substr(2, 9),
   1300,
-  9,
   "imagenespilares/postreddl.jpg"
 );
 const conitos = new Producto(
   "Conitos de Dulce de Leche",
   "postres",
+  Math.random().toString(36).substr(2, 9),
   800,
-  10,
   "imagenesventa/conitos2-min.jpg"
 );
 const bombaf = new Producto(
   "Torta Rellena",
   "postres",
+  Math.random().toString(36).substr(2, 9),
   1300,
-  11,
   "imagenesventa/bombafrut-min.jpg"
 );
 const frutilla = new Producto(
   "Tarta de Frutilla",
   "postres",
+  Math.random().toString(36).substr(2, 9),
   900,
-  12,
   "imagenesventa/tartafrut.jpg"
 );
 const lemonpie = new Producto(
   "Lemon Pie",
   "postres",
+  Math.random().toString(36).substr(2, 9),
   900,
-  13,
   "imagenesventa/lemonpie-min.jpg"
 );
 const manzana = new Producto(
   "Tarta de Manzana",
   "postres",
+  Math.random().toString(36).substr(2, 9),
   900,
-  14,
   "imagenesventa/tartamanz.jpeg"
 );
 const crumble = new Producto(
   "Crumble de Manzana",
   "postres",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  15,
   "imagenesventa/crumble-min.jpg"
 );
 const alfachoco = new Producto(
   "Alfajores de Chocolate",
   "paraelte",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  16,
   "imagenesventa/alfachocolate-min.JPG"
 );
 const maizena = new Producto(
   "Alfajorcitos de Maizena",
   "paraelte",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  17,
   "imagenesventa/maizenavert.jpg"
 );
 const nuez = new Producto(
   "Alfajorcitos de Nuez",
   "paraelte",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  18,
   "imagenesventa/alfanuez-min.jpg"
 );
 const alfajocookies = new Producto(
   "Alfacookies",
   "paraelte",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  19,
   "imagenesventa/paraelte.jpg"
 );
 const delicious = new Producto(
   "Delicious",
   "paraelte",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  20,
   "imagenesventa/delicious-min.jpg"
 );
 const mani = new Producto(
   "Cookies de Maní",
   "paraelte",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  21,
   "imagenesventa/cookiesmani-min.jpg"
 );
 const chips = new Producto(
   "Cookies con Chips",
   "paraelte",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  22,
   "imagenesventa/cookieschoco-min.jpg"
 );
 const rellenas = new Producto(
   "Cookies rellenas de limón",
   "paraelte",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  23,
   "imagenesventa/havanna-min.jpg"
 );
  
 const limon = new Producto(
   "Cookies de limón",
   "paraelte",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  24,
   "imagenesventa/limon-min.jpg"
 );
 const anillos = new Producto(
   "Anillos bañados en chocolate",
   "paraelte",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  25,
   "imagenesventa/anillos-min.JPG"
 );
 const anillosblanco = new Producto(
   "Anillos bañados en chocolate blanco",
   "paraelte",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  26,
   "imagenesventa/anilloblanco-min.jpg"
 );
 const aniz = new Producto(
   "Cookies de Aniz",
   "paraelte",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  27,
   "imagenesventa/aniz.jpg"
 );
 const pandecampo = new Producto(
   "Pan de Campo",
   "salados",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  28,
   "imagenesventa/pancampo.jpg"
 );
 const pandemolde = new Producto(
   "Pan de Molde",
   "salados",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  29,
   "imagenesventa/pandemolde.jpg"
 );
 const pandetrigo = new Producto(
   "Pan de Trigo Sarraceno",
   "salados",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  30,
   "imagenesventa/pantrigosarra.jpg"
 );
 const pandequeso = new Producto(
   "Pancitos de Queso",
   "salados",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  31,
   "imagenesventa/panqueso.jpg"
 );
 const pandecebolla = new Producto(
   "Pancitos Saborizados de Cebolla",
   "salados",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  32,
   "imagenesventa/pancebolla.jpg"
 );
 const pancitotrigo = new Producto(
   "Pancitos de Trigo Sarraceno",
   "salados",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  33,
   "imagenesventa/pancitostrigo.jpg"
 );
 const panchips = new Producto(
   "Pancitos de Chips",
   "salados",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  34,
   "imagenesventa/chips-min.jpg"
 );
 const pizza = new Producto(
   "Prepizza",
   "salados",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  35,
   "imagenesventa/pizza.jpg"
 );
 const bizcochitosgrasa = new Producto(
   "Bizcochitos de grasa",
   "salados",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  36,
   "imagenesventa/bizcochitosgrasa-min.JPG"
 );
 const bizcochitoslibritos = new Producto(
   "Bizcochitos de grasa",
   "salados",
+  Math.random().toString(36).substr(2, 9),
   1100,
-  37,
   "imagenesventa/bizcochitoslibritos-min.JPG"
 );
 productos = [];
@@ -355,36 +354,34 @@ if (paginaActual.includes("desayunos")) {
     }
 }
 }
-$("#carrito").click(() => openNav())
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  mostrarItemsEnCarrito(carrito);}
-$("#sideNavClose").click(() => closeNav())
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";}
-$(".btn").click((e)=>agregarAlCarrito(e))
-function clickeado(e) {
+$(".btn").click((e)=>agregarProducto(e))
+function agregarClick(e) {
   e.target.style.opacity = "0.8";
   e.target.style.backgroundColor = "#ffd7ba"; 
   e.target.innerHTML = "Agregado!"; 
 }
 let carrito = []; 
-function agregarAlCarrito(e) {
-  clickeado(e);
+function agregarProducto(e) {
+  agregarClick(e);
   let productoClickeado = productos.find((item) => item.id == e.target.id);
   if (localStorage.getItem("MiCarrito") != null) {
     carrito = JSON.parse(localStorage.getItem("MiCarrito"))
   } 
   carrito.push(productoClickeado);
-  mostrarItemsEnCarrito(carrito);
+  agregarItem(productoClickeado);
   localStorage.setItem("MiCarrito", JSON.stringify(carrito));
 }
-function mostrarItemsEnCarrito(array) {
-  $("#mySidenav").empty();
+function agregarItem(producto) {
+  $("#mySidenav").append(` <div class="itemCarrito"><h3>  Producto: ${producto.nombre}</h3>
+  <img src= ${producto.img} />
+  <b> $ ${producto.precio}</b> 
+  <button id="${producto.id}" class="btn-remover" > Remover</button></div>`);
+  $(".btn-remover").click((e)=>removerItem(e))
+}
+function mostrarItems(array) {
   for (const producto of array) {
-    $("#mySidenav").append(` <div class="itemCarrito"><h3> ID: ${producto.id}</h3>
+    $("#mySidenav").append(` <div class="itemCarrito"><p>  Producto: ${producto.nombre}</p>
                                  <img src= ${producto.img} />
-                                <p>  Producto: ${producto.nombre}</p>
                                <b> $ ${producto.precio}</b> 
                                <button id="${producto.id}" class="btn-remover" > Remover</button></div>`);
     $(".btn-remover").click((e)=>removerItem(e))
@@ -393,8 +390,25 @@ function mostrarItemsEnCarrito(array) {
 function removerItem(e) {
   let indexDelProducto = carrito.findIndex((item) => item.id == e.target.id);
   carrito.splice(indexDelProducto, 1);
-  mostrarItemsEnCarrito(carrito);
-  $("#cartCount").text(carrito.length);
+}
+$("#carrito").click(() => openNav())
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  if (localStorage.getItem("MiCarrito") != null) {
+    let carritoLocalStorage = []
+    let carritoTemporal = []
+    carritoLocalStorage = JSON.parse(localStorage.getItem("MiCarrito"))
+    carritoLocalStorage.forEach(item => {
+      if (!carrito.includes(item)) {
+        carritoTemporal.push(item)
+      }   
+    })
+    mostrarItems(carrito)
+  }
+} 
+$("#sideNavClose").click(() => closeNav())
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
 function eliminarProductoLocalStorage(carrito){
   let productosLS;
